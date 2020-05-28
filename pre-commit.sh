8f5998579eba2ea01ffbcd7f063824b97a71e6a1
dsaronin <dsaronin@gmail.com>
2020-05-27 22:50:49 -0700

    finishing up on pre/post-commit hooks

--- pre-commit.sh
+++ pre-commit.sh
@@ -6,11 +6,11 @@
 # afterwards, removes those flags
 # VERSION_COMMENT can be any comment which will be added to the version data file
 # for example: a projectname -- robin, bluebird, apex, etc; VERSION_COMMENT will persist
-# adds env variable: GIT_VERSION_TAG
 #
-#versionfile="VersionData.kt"
-versionfile="temp.txt"
-filepath="$(pwd)/src/$versionfile"
+versionfile="VersionData.kt"
+#versionfile="temp.txt"
+projectpath="$HOME/IdeaProjects/FatashiLinux"
+filepath="$projectpath/src/Fatashi/$versionfile"
 
 version_regex='v([0-9]+)\.([0-9]+)\.([0-9]+)'
 git_string=$(git describe --tags)
@@ -40,36 +40,44 @@ else if [ -n "$BUMP_MINOR_VERSION" ]
   fi
 fi
 
-if [ -z $VERSION_COMMENT ]
+if [ -z "$VERSION_COMMENT" ]
 then
   version_comment=""
 else
-  version_comment=$VERSION_COMMENT
+  version_comment="<$VERSION_COMMENT>"
 fi
 
 #
 
 version_string="v${major_version}.${minor_version}.${patch_version} $version_comment"
 
-echo "updating version number to: $version_string" >&2
-export GIT_VERSION_TAG=$version_string
+echo "updating version number to: $version_string in file $versionfile" >&2
 unset BUMP_MAJOR_VERSION BUMP_MINOR_VERSION
 
-
-# Working directory of a git hook is always the root of the repo
-# cat > $filepath <<EOM
-cat  <<EOM
+# cat  <<EOM
+cat > $filepath <<EOM
 package Fatashi
+// *******************************************************************************
 // NOTE: this file is automatically updated by .git/hooks/pre-commit script
-// use environmental variables: BUMP_MAJOR_VERSION, BUMP_MINOR_VERSION, VERSION_COMMENT
-//     to control logic in pre-commit
-// values are ONLY used in version.kt enum Version
+// *******************************************************************************
+// Uses environmental variables: BUMP_MAJOR_VERSION, BUMP_MINOR_VERSION, VERSION_COMMENT
+//     to control logic in pre-commit hook. Use bump.sh to auto bump appropriate level.
+// $ . ./bump.sh minor "version comment"
+// The const values below are ONLY used to initialize version.kt enum Version.
+// *******************************************************************************
+// WARNING: pre-commit and post-commit git hooks reference exact path to this file
+// ********> $filepath <********
+// **** you must change those scripts if you change the actual file location ****
+// *******************************************************************************
 
 const val _MAJOR_VERSION=$major_version
 const val _MINOR_VERSION=$minor_version
 const val _PATCH_VERSION=$patch_version
 const val _VERSION_COMMENT="$version_comment"
-}
+const val _GIT_VERSION_TAG="$version_string"
+
 EOM
+
+# echo "filepath is: $filepath"
 
 # git add $filepath
--- src/Fatashi/VersionData.kt
+++ src/Fatashi/VersionData.kt
@@ -1,8 +1,20 @@
 package Fatashi
+// *******************************************************************************
 // NOTE: this file is automatically updated by .git/hooks/pre-commit script
-// values are ONLY used in version.kt enum Version
+// *******************************************************************************
+// Uses environmental variables: BUMP_MAJOR_VERSION, BUMP_MINOR_VERSION, VERSION_COMMENT
+//     to control logic in pre-commit hook. Use bump.sh to auto bump appropriate level.
+// $ . ./bump.sh minor "version comment"
+// The const values below are ONLY used to initialize version.kt enum Version.
+// *******************************************************************************
+// WARNING: pre-commit and post-commit git hooks reference exact path to this file
+// ********> /home/daudi/IdeaProjects/FatashiLinux/src/Fatashi/VersionData.kt <********
+// **** you must change those scripts if you change the actual file location ****
+// *******************************************************************************
 
 const val _MAJOR_VERSION=0
 const val _MINOR_VERSION=0
-const val _PATCH_VERSION=5
-const val _VERSION_COMMENT=""
+const val _PATCH_VERSION=7
+const val _VERSION_COMMENT="<wildblue>"
+const val _GIT_VERSION_TAG="v0.0.7 <wildblue>"
+
